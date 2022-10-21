0f09e4d3f0645b7781120657e93ae1e8dc1b2462
nasreepattani <s64309010009@ptnicec.ac.th>
2022-10-21 11:29:01 +0700

    ส่งไฟล๋ test ครั้งที่1

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+   echo"Nasree Langi";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+<body>
+    <br> Information Technology <br>
+    <?php
+       echo "Pattani";
+    ?>
+    <br>
+</body>
+</html>
+<?php
+    echo "Thailand";
+?>
